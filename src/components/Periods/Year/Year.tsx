3ee4e75641b3f418cdb303f7a27fe50fbab52dde
vmakal1989 <vmakal1989@gmail.com>
2021-01-11 00:36:30 +0300

    build

--- src/components/Periods/Year/Year.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FunctionComponent} from 'react'
-
-export const Year: FunctionComponent = () => {
-	return (
-		<div>
-			Year
-		</div>
-	)
-}
--- src/types/types.ts
+++
@@ -1,9 +0,0 @@
-
-export interface elementsOfDayType {
-    name: string
-    number: number | string
-    note: number
-    current: boolean
-    isHoliday: boolean
-    date: string
-}
